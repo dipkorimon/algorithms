f98d869968ec69147ae3392b9af910cd1a9d0ab6
dipkorimon <dipkorimon@gmail.com>
2020-11-29 21:04:31 +0600

    Add files via upload

---
+++ dfs.cpp
@@ -0,0 +1,74 @@
+#define LOCAL
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template <typename A, typename B>
+ostream& operator <<(ostream& out, const pair<A, B>& a) {
+  out << "(" << a.first << "," << a.second << ")";
+  return out;
+}
+template <typename T, size_t N>
+ostream& operator <<(ostream& out, const array<T, N>& a) {
+  out << "["; bool first = true;
+  for (auto& v : a) { out << (first ? "" : ", "); out << v; first = 0;} out << "]";
+  return out;
+}
+template <typename T>
+ostream& operator <<(ostream& out, const vector<T>& a) {
+  out << "["; bool first = true;
+  for (auto& v : a) { out << (first ? "" : ", "); out << v; first = 0;} out << "]";
+  return out;
+}
+template <typename T, class Cmp>
+ostream& operator <<(ostream& out, const set<T, Cmp>& a) {
+  out << "{"; bool first = true;
+  for (auto& v : a) { out << (first ? "" : ", "); out << v; first = 0;} out << "}";
+  return out;
+}
+template <typename U, typename T, class Cmp>
+ostream& operator <<(ostream& out, const map<U, T, Cmp>& a) {
+  out << "{"; bool first = true;
+  for (auto& p : a) { out << (first ? "" : ", "); out << p.first << ":" << p.second; first = 0;} out << "}";
+  return out;
+}
+#ifdef LOCAL
+#define trace(...) __f(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define trace(...) 42
+#endif
+template <typename Arg1>
+void __f(const char* name, Arg1&& arg1){
+  cerr << name << ": " << arg1 << endl;
+}
+template <typename Arg1, typename... Args>
+void __f(const char* names, Arg1&& arg1, Args&&... args){
+  const char* comma = strchr(names + 1, ',');
+  cerr.write(names, comma - names) << ": " << arg1 << " |";
+  __f(comma + 1, args...);
+}
+
+vector<vector<int>> adj; // graph represented as an adjacency list
+int n; //number of vertices
+vector<int> color;
+vector<int> time_in, time_out;
+int dfs_timer = 0;
+
+void dfs(int v) {
+	time_in[v] = dfs_timer++;
+	color[v] = 1;
+	for (int u : adj[v]) {
+		if (color[u] == 0) {
+			dfs(u);
+		}
+	}
+	color[v] = 2;
+	time_out[v] = dfs_timer++;
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	
+	return 0;
+}
