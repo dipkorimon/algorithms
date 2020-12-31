d3ef4e41bce94d706fc5af0815b0b9d5cc4164d8
dipkorimon <dipkorimon@gmail.com>
2020-12-31 22:26:21 +0600

    Update 2d_grid.cpp

--- 2d_grid.cpp
+++ 2d_grid.cpp
@@ -54,61 +54,59 @@ bool visited[5][5];
 int dx[] = {-1, 1,  0, 0, -1, -1,  1, 1};
 int dy[] = {0,  0, -1, 1, -1,  1, -1, 1};
 
-bool valid(int x, int y){
-    return (x >= 0 && x < 5 && y >= 0 && y < 5 && !visited[x][y]);
+//~ knight moves
+//~ int kx[] = {1, 1, 2, 2, -1, -1, -2, -2};
+//~ int ky[] = {2, -2, 1, -1, 2, -2, 1, -1};
+
+bool valid(int x, int y) {
+	return (x >= 0 && x < 5 && y >= 0 && y < 5 && !visited[x][y]);
 }
 
-void dfs(int x, int y){
-    // visit the cell
-    if (visited[x][y]) return; // this line is not necessary anymore since we are checking validity before calling the function
-    visited[x][y] = 1;
-
-    for (int i = 0; i < 8; i++){
-       int next_x = x + dx[i];
-       int next_y = y + dy[i];
-
-        if (valid(next_x, next_y)){
-            dfs(next_x, next_y);
-        }
-   }
+void dfs(int x, int y) {
+	//~ visit the cell
+	if (!visited[x][y]) return;
+	visited[x][y] = 1;
+	for (int i = 0; i < 8; i++) {
+		int next_x = dx[i] + x;
+		int next_y = dy[i] + y;
+		if (valid(next_x, next_y)) {
+			dfs(next_x, next_y);
+		}
+	}
 }
 
-struct node{
-    int x;
-    int y;
+struct node {
+	int x, y;
 };
 
-void bfs(int sx, int sy){
-    queue < node > Q;
-    Q.push({sx, sy});
-    visited[sx][sy] = 1;
-
-    while(!Q.empty()){
-        int x = Q.front().x;
-        int y = Q.front().y;
-        Q.pop();
-
-        for (int i = 0; i < 8; i++){
-            int next_x = x + dx[i];
-            int next_y = y + dy[i];
-
-            if (valid(next_x, next_y)){
-                visited[next_x][next_y] = 1;
-                Q.push({next_x, next_y});
-            }
-       }
-    }
+void bfs(int sx, int sy) {
+	queue<node> Q;
+	Q.push({sx, sy});
+	visited[sx][sy] = 1;
+	while (!Q.empty()) {
+		int x = Q.front().x;
+		int y = Q.front().y;
+		Q.pop();
+		for (int i = 0; i < 8; i++) {
+			int next_x = dx[i] + x;
+			int next_y = dy[i] + y;
+			if (valid(next_x, next_y)) {
+				visited[next_x][next_y] = 1;
+				Q.push({next_x, next_y});
+			}
+		}
+	}
 }
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(0);
 	bfs(2, 2);
-    for (int i = 0; i < 5; i++){
-        for (int j = 0; j < 5; j++){
-            cout << visited[i][j] << " ";
-        }
-        cout << endl;
-    }
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j < 5; j++) {
+			cout << visited[i][j] << " ";
+		}
+		cout << "\n";
+	}
 	return 0;
 }
